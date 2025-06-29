2893155b0820d1a3ccdb59c4a774d5930e697647
CyanLimeOfficial <jamesataturuk@gmail.com>
2025-06-29 08:34:03 +0800

    Test Deployement

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const HomePage: FC = () => {
                 <div className="absolute inset-0 h-full w-full bg-slate-900 bg-[linear-gradient(to_right,#80808012_1px,transparent_1px),linear-gradient(to_bottom,#80808012_1px,transparent_1px)] bg-[size:24px_24px]"></div>
                 <div className="relative z-10">
                     <Image 
-                        src="https://placehold.co/150x150/ffffff/1e293b?text=JL" 
+                        src="https://placehold.co/150x150/ffffff/1e293b?text=DL" 
                         alt="Dexter Lanzarrote" 
                         width={160}
                         height={160}
